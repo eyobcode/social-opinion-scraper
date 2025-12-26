919e7dfe6b339cb8ca1a5b5cf53bf02c6820472d
eyobScript <eyobbmulugeta@gmail.com>
2025-12-26 14:29:41 +0300

    base class

--- platforms/base.py
+++ platforms/base.py
@@ -1,10 +1,12 @@
 from core.utils import ScraperUtils
+from core.insta_utils import InstaUtils
 
 class ScraperBase:
     def __init__(self, headless: bool = True, user_data_dir: str = None):
         self.headless = headless
         self.user_data_dir = user_data_dir
         self.utils = ScraperUtils
+        self.insta_utils = InstaUtils
 
     def close(self):
         pass
